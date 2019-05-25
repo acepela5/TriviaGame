e1b5e372d4f75ed4fd1036791d1c420d2201a74c
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-05-25 17:19:01 -0500

    questions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,105 @@
 $(document).ready(function() {
-// variables
-var number = 30;
-var intervalId;
-// start button
-$("#startButton").on("click", runTimer);
-// timer that counts down
-function runTimer() {
+  // variables
+  var number = 10;
+  var intervalId;
+
+  var correctAnswers = 0;
+  var incorrectAnswers = 0;
+  var unanswered = 0;
+  var index = 0;
+
+  // questionn / answers / correct answerd
+  var quiz = [
+    { question: "What are the names of Bruce Wayne's parents?", answers: ["James and Dorthy", "Thomas and Martha", "John and Elizabeth", "Anthony and Patricia"], correct: 1, },
+    { question: "Which of these villains was introduced first?", answers: ["The Riddler", "Penguin", "Killer Croc", "Catwoman"], correct: 3 },
+    { question: "What is the original identity of Two-Face?", answers: ["Harvey Dent", "Timmy Pickles", "Eli Pennyworth", "Michael Gill"], correct: 0 },
+    { question: "Which character becomes Oracle after being shot?", answers: ["Catwoman", "Poison Ivy", "Batgirl", "Batwoman"], correct: 2 },
+    { question: "Which of the following characters is an actual Batman villian?", answers: ["Calendar Man", "Ice Man", "Phone Man", "Alligator Man"], correct: 0 }
+];
+
+  console.log(quiz);
+  console.log(quiz[0].question);
+  console.log(quiz[1].answers[1]);
+
+  function win(){
+      var imageWin = 
+      correctAnswers++
+      index++
+    setTimeout(function(){}, 3000);
+  }
+  function lose(){
+    var imageLose = 
+    incorrectAnswers++
+    index++
+  setTimeout(function(){}, 3000);
+  }
+  function unanswered(){
+      var imageDissapointed =
+      unanswered++
+      index++
+    setTimeout(function(){}, 3000);
+  }
+  function outOfTime(){
+      var imageOutOfTime =
+      index++
+      setTimeout(function(){}, 3000);
+  }
+  function displayScoreBoard(){
+      var scoreBoard = $("<div>");
+      scoreBoard.html(correctAnswers + incorrectAnswers + unanswered);
+  }
+
+  function showTriva() {
+    $("#showQuestion").html(quiz[index].question);
+    for (var i = 0; i < quiz[index].answers.length; i++) {
+      var answerButton= $("<button>");
+      answerButton.html(quiz[index].answers[i]);
+      answerButton.attr("id", quiz[index].answers[i]);
+      $("#showPossibleAnswers").append(answerButton + "<br>");
+
+        $(answerButton).on("click", function(){
+            var userResponse = $(this).attr("id")
+            if(userResponse===quiz[index].correct){
+                win();
+            }
+            else{
+                lose();
+            }
+            if(userResponse !== quiz[index].correct || number===0){
+                unanswered();
+            }
+            
+        })
+        
+      }
+      
+    }
+    
+  showTriva();
+  displayScoreBoard();
+
+// timer
+$("#startButton").on("click", run);
+
+  function run() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-}
-function decrement() {
-    number --;
-    $("#countDownNumber").html("<h2>" + number + "</h2>");
-    console.log(number);
-// when the timer reaches 0, screen changes to show # of correct, incorrect, and unanswered questions
-    if (number===0) {
+  }
+
+  function decrement() {
+    number--;
+    $("#countDown").html("<h2>" + number + "</h2>");
+    if (number === 0) {
         stop();
-        alert("Time's Up");
+        outOfTime();
+        displayScoreBoard();
     }
-}
-
-runTimer();
-// questions are either true/false or multiple choice
-
-// player cannot choose more than 1 answer
-
-// if all questions answered, screen changes to show # of correct, incorrect. and unanswered questions
+  }
+  function stop() {
+    clearInterval(intervalId);
+  }
+//   run();
 
 
+  
 });
