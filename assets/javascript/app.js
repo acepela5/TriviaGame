cd387bbd185f84110730cd9bcdd0155e8f1e29af
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-05-28 22:09:33 -0500

    reset fixed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // variables
+  // variables
   var number = 11;
   var intervalId;
 
@@ -18,12 +18,12 @@ $(document).ready(function() {
         "John and Elizabeth",
         "Anthony and Patricia"
       ],
-      correct: 1,
+      correct: 1
     },
     {
       question: "Which of these villains was introduced first?",
       answers: ["The Riddler", "Penguin", "Killer Croc", "Catwoman"],
-      correct: 3,
+      correct: 3
     },
     {
       question: "What is the original identity of Two-Face?",
@@ -33,18 +33,18 @@ $(document).ready(function() {
         "Eli Pennyworth",
         "Michael Gill"
       ],
-      correct: 0,
+      correct: 0
     },
     {
       question: "Which character becomes Oracle after being shot?",
       answers: ["Catwoman", "Poison Ivy", "Batgirl", "Batwoman"],
-      correct: 2,
+      correct: 2
     },
     {
       question:
         "Which of the following characters is an actual Batman villian?",
       answers: ["Calendar Man", "Ice Man", "Phone Man", "Alligator Man"],
-      correct: 0,
+      correct: 0
     }
   ];
 
@@ -57,98 +57,114 @@ $(document).ready(function() {
 
   // functions for the scoreboard
   function win() {
-    $("#showQuestion").empty()
-   $("#showPossibleAnswers").empty()
-   $("#showPossibleAnswers").html("<img src=./assets/images/symbol.gif>");
+    $("#showQuestion").empty();
+    $("#showPossibleAnswers").empty();
+    $("#showPossibleAnswers").html("<img src=./assets/images/symbol.gif>");
     correctAnswers++;
-    console.log('CA', correctAnswers);
+    console.log("CA", correctAnswers);
     setTimeout(run, 3000);
-   
   }
   function lose() {
-    $("#showQuestion").empty() 
-    $("#showPossibleAnswers").empty()
+    $("#showQuestion").empty();
+    $("#showPossibleAnswers").empty();
     $("#showPossibleAnswers").html("<img src=./assets/images/sadbat1.gif />");
     var rightAnswer = $("#showQuestion");
-    rightAnswer.append("<p>@The correct answer is: " + quiz[index].answers[quiz[index].correct]);
+    rightAnswer.append(
+      "<p>@The correct answer is: " + quiz[index].answers[quiz[index].correct]
+    );
     incorrectAnswers++;
     setTimeout(run, 3000);
-    
-
   }
   function outOfTime() {
-    stop() 
-    $("#showQuestion").empty() 
-    $("#showPossibleAnswers").empty()
-    $("#showPossibleAnswers").html("<img src=./assets/images/angrybat.gif>")
+    stop();
+    $("#showQuestion").empty();
+    $("#showPossibleAnswers").empty();
+    $("#showPossibleAnswers").html("<img src=./assets/images/angrybat.gif>");
     var rightAnswer = $("#showQuestion");
-    rightAnswer.append("<p>@The correct answer is: " + quiz[index].answers[quiz[index].correct]);
+    rightAnswer.append(
+      "<p>@The correct answer is: " + quiz[index].answers[quiz[index].correct]
+    );
     unanswered++;
     setTimeout(run, 3000);
   }
 
-  function displayScoreBoard() {
-    var scoreBoard = $("#quizArea");
-    scoreBoard.html("<p>@Wins: " + correctAnswers + "</p>").css("font-size","40px");
-    scoreBoard.append("<p>@Losses: " + incorrectAnswers + "</p>").css("font-size","40px");
-    scoreBoard.append("<p>@Unanswered: " + unanswered + "</p>").css("font-size","35px");
-  }
-// to play the game
+  // to play the game
   function showTriva() {
     $("#showPossibleAnswers").empty();
-    number=11;
+    number = 11;
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-    console.log(("index: ", index))
+    console.log(("index: ", index));
     $("#showQuestion").html(quiz[index].question);
     for (var i = 0; i < quiz[index].answers.length; i++) {
       var answerButton = $("<button>");
       $("#showPossibleAnswers").append(answerButton);
       answerButton.html(quiz[index].answers[i]);
       answerButton.attr("id", quiz[index].answers[i]);
-    
-    $(answerButton).on("click", function() {
-        clearInterval(intervalId)
-      var userResponse = $(this).attr("id");
-      console.log('CC', userResponse);
-      console.log('AA', quiz[index].answers[quiz[index].correct]);
-      var qAnswer = quiz[index].answers[quiz[index].correct]
-      console.log('BB', quiz[index].correct[i])
-      if (userResponse === qAnswer) {
-          console.log('i won');
-          
-        win();
-      } else {
-        stop();
-        lose();
-      }
-      }
-    )};
-}
 
- 
+      $(answerButton).on("click", function() {
+        clearInterval(intervalId);
+        var userResponse = $(this).attr("id");
+        console.log("CC", userResponse);
+        console.log("AA", quiz[index].answers[quiz[index].correct]);
+        var qAnswer = quiz[index].answers[quiz[index].correct];
+        console.log("BB", quiz[index].correct[i]);
+        if (userResponse === qAnswer) {
+          console.log("i won");
+
+          win();
+        } else {
+          stop();
+          lose();
+        }
+      });
+    }
+  }
+  var restartButton = $("#restart");
+  $("#restart").append(restartButton);
+  $(restartButton).on("click", restartGame);
+
+  function restartGame() {
+    correctAnswers = 0;
+    incorrectAnswers = 0;
+    unanswered = 0;
+    index=0;
+    showTriva();
+  }
+
+  function displayScoreBoard() {
+    var scoreBoard = $("#quizArea");
+    scoreBoard
+      .html("<p>@Wins: " + correctAnswers + "</p>")
+      .css("font-size", "40px");
+    scoreBoard
+      .append("<p>@Losses: " + incorrectAnswers + "</p>")
+      .css("font-size", "40px");
+    scoreBoard
+      .append("<p>@Unanswered: " + unanswered + "</p>")
+      .css("font-size", "35px");
+  }
+
   // timer used from interval activity
   function run() {
-    showTriva()
-    index++
-    if (index >= quiz.length){
-        displayScoreBoard()
-    }
-    else{
     showTriva();
+    index++;
+    if (index >= quiz.length) {
+      displayScoreBoard();
+    } else {
+      showTriva();
     }
   }
 
   function stop() {
     clearInterval(intervalId);
-
   }
   // making the number decrease
   function decrement() {
     number--;
     $("#countDown").html("<h1>" + number + "</h1>");
     if (number === 0) {
-     outOfTime();
+      outOfTime();
     }
   }
 });
