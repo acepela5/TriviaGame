72955e8db5b776a1df11350269dc587ea15da447
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-05-28 19:55:24 -0500

    cleaned up code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
   var unanswered = 0;
   var index = 0;
 
-  // questionn / answers / correct answerd
+  // questionn / answers / correct answer
   var quiz = [
     {
       question: "What are the names of Bruce Wayne's parents?",
@@ -19,13 +19,11 @@ $(document).ready(function() {
         "Anthony and Patricia"
       ],
       correct: 1,
-      img: "../assets/images/symbol.gif"
     },
     {
       question: "Which of these villains was introduced first?",
       answers: ["The Riddler", "Penguin", "Killer Croc", "Catwoman"],
       correct: 3,
-      animate: "https://giphy.com/gifs/TlK63EvSQlC3joA3Zja/html5"
     },
     {
       question: "What is the original identity of Two-Face?",
@@ -36,20 +34,17 @@ $(document).ready(function() {
         "Michael Gill"
       ],
       correct: 0,
-      animate: "https://giphy.com/gifs/y4G4trcBoUgWk/html5"
     },
     {
       question: "Which character becomes Oracle after being shot?",
       answers: ["Catwoman", "Poison Ivy", "Batgirl", "Batwoman"],
       correct: 2,
-      animate: "https://giphy.com/gifs/bJaxgozZusl2g/html5"
     },
     {
       question:
         "Which of the following characters is an actual Batman villian?",
       answers: ["Calendar Man", "Ice Man", "Phone Man", "Alligator Man"],
       correct: 0,
-      animate: "https://giphy.com/gifs/3ohzdCTBKqn8nviNdm/html5"
     }
   ];
 
@@ -98,7 +93,7 @@ $(document).ready(function() {
     scoreBoard.append("<p>@Losses: " + incorrectAnswers + "</p>").css("font-size","40px");
     scoreBoard.append("<p>@Unanswered: " + unanswered + "</p>").css("font-size","35px");
   }
-
+// to play the game
   function showTriva() {
     $("#showPossibleAnswers").empty();
     number=11;
@@ -107,7 +102,6 @@ $(document).ready(function() {
     console.log(("index: ", index))
     $("#showQuestion").html(quiz[index].question);
     for (var i = 0; i < quiz[index].answers.length; i++) {
-        // $("#showPossibleAnswers").append(quiz[index].answers[i] + "<br>");  
       var answerButton = $("<button>");
       $("#showPossibleAnswers").append(answerButton);
       answerButton.html(quiz[index].answers[i]);
